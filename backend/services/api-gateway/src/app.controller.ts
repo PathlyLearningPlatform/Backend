cb1833418cd1e54c9956df8c770a4b4495d312a0
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-11-29 21:56:52 +0100

    formatting

--- backend/services/api-gateway/src/app.controller.ts
+++ backend/services/api-gateway/src/app.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 
 @Controller()
 export class AppController {
-  @Get('healthcheck')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  healthcheck() {}
+	@Get('healthcheck')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	healthcheck() {}
 }
