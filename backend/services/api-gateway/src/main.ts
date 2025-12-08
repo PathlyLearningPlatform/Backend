c28b74b4bce9916b0137fb5a79b908d1b2dc0961
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-08 20:54:26 +0100

    using logger from common library and getting app port from config

--- backend/services/api-gateway/src/main.ts
+++ backend/services/api-gateway/src/main.ts
@@ -1,9 +1,20 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { AppLogger } from 'common/index.js'
 import { AppModule } from './app.module'
+import type { AppConfig } from './infra/common/types'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+	const app = await NestFactory.create(AppModule, {
+		bufferLogs: true,
+	})
+
+	const configService = app.get(ConfigService)
+	const appConfig = configService.get<AppConfig['app']>('app')!
+
+	app.useLogger(new AppLogger())
+
+	await app.listen(appConfig.port)
 }
 
 bootstrap()
