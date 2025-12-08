ba837f8c374c312998bdf57c8eda943ec78b9ccf
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-08 20:56:35 +0100

    imported config module, app logger module and registered global interceptor and exception filter

--- backend/services/api-gateway/src/app.module.ts
+++ backend/services/api-gateway/src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
+import { AppLoggerModule, HttpExceptionFilter, HttpRequestInterceptor } from 'common/index.js'
 import { AppController } from './app.controller'
+import { validateConfig } from './infra/config'
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			ignoreEnvFile: true,
+			validate: validateConfig,
+		}),
+		AppLoggerModule.register({
+			isGlobal: true,
+		}),
+	],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: HttpRequestInterceptor,
+		},
+		{
+			provide: APP_FILTER,
+			useClass: HttpExceptionFilter,
+		}
+	],
 	controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
