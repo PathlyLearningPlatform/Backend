c8d3033acd443bdce5180db8181e5bbda3dca847
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-08 23:07:42 +0100

    app config validation function

--- backend/services/paths/src/infra/config/validate.ts
+++ backend/services/paths/src/infra/config/validate.ts
@@ -2,24 +2,24 @@ import { appConfigSchema } from '@infra/common/schemas';
 import { ConfigException, parseIntOrReturn } from 'common/index.js';
 
 export function validateConfig(config: Record<string, unknown>) {
-  const transformedConfig = {
-    app: {
-      port: parseIntOrReturn(config.PORT),
-    },
-    db: {
-      host: config.DB_HOST,
-      name: config.DB_NAME,
-      port: parseIntOrReturn(config.DB_PORT),
-      user: config.DB_USER,
-      password: config.DB_PASSWORD,
-    },
-  };
+	const transformedConfig = {
+		app: {
+			port: parseIntOrReturn(config.PORT),
+		},
+		db: {
+			host: config.DB_HOST,
+			name: config.DB_NAME,
+			port: parseIntOrReturn(config.DB_PORT),
+			user: config.DB_USER,
+			password: config.DB_PASSWORD,
+		},
+	};
 
-  const result = appConfigSchema.safeParse(transformedConfig);
+	const result = appConfigSchema.safeParse(transformedConfig);
 
-  if (result.success) {
-    return result.data;
-  }
+	if (result.success) {
+		return result.data;
+	}
 
-  throw new ConfigException(result.error.message, result.error);
+	throw new ConfigException(result.error.message, result.error);
 }
