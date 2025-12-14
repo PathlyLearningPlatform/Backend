59d6401354934ddf63d6f7dc5ea1ab56de07e1dc
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-14 09:13:36 +0100

    tests for paths repository

---
+++ backend/services/paths/src/infra/paths/tests/paths.repository.spec.ts
@@ -0,0 +1,139 @@
+import { DbService } from '@/infra/db/db.service';
+import { Test } from '@nestjs/testing';
+import { mockedDb, mockedDbService } from './mocks/db.mock';
+import { PathsRepository } from '../paths.repository';
+import { mockedDbPath, mockedPath } from './mocks/paths.mock';
+import {
+	mockedCreateCommand,
+	mockedFindCommand,
+	mockedFindOneCommand,
+	mockedRemoveCommand,
+	mockedUpdateCommand,
+} from '@/app/paths/use-cases/tests/mocks/commands.mock';
+import { DbException } from '@pathly-backend/common/index.js';
+
+describe('PathsRepository', () => {
+	let pathsRepository: PathsRepository;
+
+	beforeEach(async () => {
+		const moduleRef = await Test.createTestingModule({
+			providers: [mockedDbService, PathsRepository],
+		}).compile();
+
+		pathsRepository = moduleRef.get(PathsRepository);
+	});
+
+	describe('find', () => {
+		it('should return an array of paths', async () => {
+			mockedDb.offset.mockResolvedValueOnce([mockedDbPath]);
+
+			const result = await pathsRepository.find(mockedFindCommand);
+
+			expect(result).toEqual([mockedPath]);
+		});
+
+		it('should throw DbException', async () => {
+			mockedDb.offset.mockRejectedValueOnce(new Error());
+
+			const promise = pathsRepository.find(mockedFindCommand);
+
+			await expect(promise).rejects.toThrow(DbException);
+		});
+	});
+
+	describe('findOne', () => {
+		it('should return a path', async () => {
+			mockedDb.where.mockResolvedValueOnce([mockedDbPath]);
+
+			const result = await pathsRepository.findOne(mockedFindOneCommand);
+
+			expect(result).toEqual(mockedPath);
+		});
+
+		it('should return null', async () => {
+			mockedDb.where.mockResolvedValueOnce([]);
+
+			const result = await pathsRepository.findOne(mockedFindOneCommand);
+
+			expect(result).toEqual(null);
+		});
+
+		it('should throw DbException', async () => {
+			mockedDb.where.mockResolvedValueOnce(new Error());
+
+			const promise = pathsRepository.findOne(mockedFindOneCommand);
+
+			await expect(promise).rejects.toThrow(DbException);
+		});
+	});
+
+	describe('create', () => {
+		it('should return created path', async () => {
+			mockedDb.returning.mockResolvedValueOnce([mockedDbPath]);
+
+			const result = await pathsRepository.create(mockedCreateCommand);
+
+			expect(result).toEqual(mockedPath);
+		});
+
+		it('should throw DbException', async () => {
+			mockedDb.returning.mockResolvedValueOnce(new Error());
+
+			const promise = pathsRepository.create(mockedCreateCommand);
+
+			await expect(promise).rejects.toThrow(DbException);
+		});
+	});
+
+	describe('update', () => {
+		it('should return updated path', async () => {
+			mockedDb.returning.mockResolvedValueOnce([mockedDbPath]);
+
+			const result = await pathsRepository.update(mockedUpdateCommand);
+
+			expect(result).toEqual(mockedPath);
+		});
+
+		it('should return null', async () => {
+			mockedDb.returning.mockResolvedValueOnce([]);
+
+			const result = await pathsRepository.update(mockedUpdateCommand);
+
+			expect(result).toEqual(null);
+		});
+
+		it('should throw DbException', async () => {
+			mockedDb.returning.mockResolvedValueOnce(new Error());
+
+			const promise = pathsRepository.update(mockedUpdateCommand);
+
+			await expect(promise).rejects.toThrow(DbException);
+		});
+	});
+
+	describe('remove', () => {
+		it('should return removed path', async () => {
+			mockedDb.returning.mockResolvedValueOnce([mockedDbPath]);
+
+			const result = await pathsRepository.remove(mockedRemoveCommand);
+
+			expect(result).toEqual(mockedPath);
+		});
+
+		it('should return null', async () => {
+			mockedDb.returning.mockResolvedValueOnce([]);
+
+			const result = await pathsRepository.remove(mockedRemoveCommand);
+
+			expect(result).toEqual(null);
+		});
+
+		it('should throw DbException', async () => {
+			mockedDb.returning.mockResolvedValueOnce(new Error());
+
+			const promise = pathsRepository.remove(mockedRemoveCommand);
+
+			await expect(promise).rejects.toThrow(DbException);
+		});
+	});
+});
