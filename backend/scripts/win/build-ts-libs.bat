5290aa91ad3e266b36967c5ec2ce3e9c7af15837
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-14 14:38:47 +0100

    added api gateway service to build-ts-libs and gen-contracts scripts

--- backend/scripts/win/build-ts-libs.bat
+++ backend/scripts/win/build-ts-libs.bat
@@ -3,3 +3,5 @@ npm run build -w libs/contracts -w libs/ts/common
 
 # for docker containers
 docker compose exec -w /usr/src/app paths npm run build -w libs/contracts -w libs/ts/common
+
+docker compose exec -w /usr/src/app api-gateway npm run build -w libs/contracts -w libs/ts/common
--- backend/scripts/win/gen-contracts.bat
+++ backend/scripts/win/gen-contracts.bat
@@ -3,3 +3,5 @@ npm run gen -w libs/contracts
 
 # for docker containers
 docker compose exec -w /usr/src/app paths npm run gen -w libs/contracts
+
+docker compose exec -w /usr/src/app api-gateway npm run gen -w libs/contracts
