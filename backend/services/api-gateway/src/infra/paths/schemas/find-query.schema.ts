021a662ad9ddfa76027d11195ca53e053750b896
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-18 13:52:33 +0100

    changed type of limit and page fields from z.int32 to z.coerce.number

--- backend/services/api-gateway/src/infra/paths/schemas/find-query.schema.ts
+++ backend/services/api-gateway/src/infra/paths/schemas/find-query.schema.ts
@@ -5,12 +5,15 @@ import { PathsApiConstraints } from '../enums'
 
 export const findPathsQuerySchema = z
 	.object({
-		limit: z
-			.int32()
+		limit: z.coerce
+			.number()
 			.min(PathsApiConstraints.MIN_LIMIT)
 			.max(PathsApiConstraints.MAX_LIMIT)
 			.default(PathsApiConstraints.DEFAULT_LIMIT),
-		page: z.int32().nonnegative().default(PathsApiConstraints.DEFAULT_PAGE),
+		page: z.coerce
+			.number()
+			.nonnegative()
+			.default(PathsApiConstraints.DEFAULT_PAGE),
 		orderBy: z.enum(PathsOrderByFields).default(PathsOrderByFields.CREATED_AT),
 		sortType: z.enum(SortType).default(SortType.DESC),
 	})
