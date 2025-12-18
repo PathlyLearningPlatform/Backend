7ccab0a268b410cc53b00cd697bba26290ab6f36
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-18 15:00:04 +0100

    added mockedClientPath

--- backend/services/paths/src/infra/paths/tests/mocks/paths.mock.ts
+++ backend/services/paths/src/infra/paths/tests/mocks/paths.mock.ts
@@ -1,11 +1,13 @@
-import type { Path } from '@pathly-backend/contracts/paths/v1/paths.js';
+import type { Path as ClientPath } from '@pathly-backend/contracts/paths/v1/paths.js';
 import type { DbPath } from '../../types';
+import { Path } from '@/domain/paths/entities';
+import { nullToEmptyString } from '@pathly-backend/common/index.js';
 
 export const mockedPath: Path = {
 	id: '4692163a-3f1e-46f1-abd2-8c47f05a469c',
 	createdAt: '2025-12-10T20:00:00.000Z',
 	updatedAt: '2025-12-10T20:00:00.000Z',
-	description: '',
+	description: null,
 	name: 'Web development for begginers',
 };
 export const mockedUpdatedPath: Path = {
@@ -17,4 +19,9 @@ export const mockedDbPath: DbPath = {
 	...mockedPath,
 	createdAt: new Date(mockedPath.createdAt),
 	updatedAt: new Date(mockedPath.updatedAt),
+};
+
+export const mockedClientPath: ClientPath = {
+	...mockedPath,
+	description: nullToEmptyString(mockedPath.description),
 };
