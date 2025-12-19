06dcec5bbc94288a4fde31b9bbccd59f8d7b81ad
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-19 21:20:29 +0100

    added apiCode field

--- backend/libs/ts/common/src/dtos/grpc-error.dto.ts
+++ backend/libs/ts/common/src/dtos/grpc-error.dto.ts
@@ -3,11 +3,13 @@ export class GrpcErrorDto {
   public readonly timestamp: string;
   public readonly details: unknown | null;
   public readonly code: number;
+  public readonly apiCode: number | null;
 
-  constructor(message: string, code: number, details: unknown = null) {
+  constructor(message: string, code: number, apiCode: number | null = null, details: unknown = null) {
     this.message = message;
     this.details = details;
     this.code = code;
+    this.apiCode = apiCode
     this.timestamp = new Date().toISOString()
   }
 }
