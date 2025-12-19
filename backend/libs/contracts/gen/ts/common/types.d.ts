92960c1d06c6683e468e938af0ddc34f4a8ff018
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-19 15:51:14 +0100

    generated contracts

--- backend/libs/contracts/gen/ts/common/types.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const protobufPackage = "proto.common";
-export declare enum SortType {
-    ASC = 0,
-    DESC = 1
-}
-export declare const PROTO_COMMON_PACKAGE_NAME = "proto.common";
-//# sourceMappingURL=types.d.ts.map
