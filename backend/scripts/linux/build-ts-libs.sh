a7ecbf099fcc763ffd4fd8217ad89b4a0ffeb70c
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-22 22:49:00 +0100

    restart services after building libs

--- backend/scripts/linux/build-ts-libs.sh
+++ backend/scripts/linux/build-ts-libs.sh
@@ -7,3 +7,5 @@ npm run build -w libs/contracts -w libs/ts/common
 docker compose exec -w /usr/src/app paths npm run build -w libs/contracts -w libs/ts/common
 
 docker compose exec -w /usr/src/app api-gateway npm run build -w libs/contracts -w libs/ts/common
+
+docker compose restart paths api-gateway
