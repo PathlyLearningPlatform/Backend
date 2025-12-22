4934da3b4c30bf15fbd19c910c1512965d93a169
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2025-12-22 22:47:20 +0100

    improvements

--- backend/services/paths/src/domain/paths/commands/create.command.ts
+++ backend/services/paths/src/domain/paths/commands/create.command.ts
@@ -1,19 +1,19 @@
-import { IntersectionType, OmitType, PartialType } from '@nestjs/mapped-types';
+import {
+	IntersectionType,
+	OmitType,
+	PartialType,
+	PickType,
+} from '@nestjs/mapped-types';
 import { Path } from '../entities';
 
-class RequiredFields extends OmitType(Path, [
-	'createdAt',
-	'updatedAt',
-	'id',
-	'description',
-]) {}
-class OptionalFields extends PartialType(
-	OmitType(Path, ['createdAt', 'updatedAt', 'id', 'name']),
-) {}
-class Fields extends IntersectionType(RequiredFields, OptionalFields) {}
+class RequiredFields extends PickType(Path, ['name']) {}
+class AllowedFields extends OmitType(Path, ['createdAt', 'updatedAt', 'id']) {}
 
 /**
  * @description
  * This class represents data needed (required and optional) to create path entity.
  */
-export class CreatePathCommand extends Fields {}
+export class CreatePathCommand extends IntersectionType(
+	RequiredFields,
+	PartialType(AllowedFields),
+) {}
