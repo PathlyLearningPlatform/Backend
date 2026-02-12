ccef21a5998518549b7004b8194d0374a2e22363
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-02-12 11:03:40 +0100

    updated build-libs-ts scripts to include libs/ts/core library

--- scripts/linux/build-ts-libs.sh
+++ scripts/linux/build-ts-libs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # build contracts and ts libs locally
-npm run build -w libs/contracts -w libs/ts/common
+npm run build -w libs/contracts -w libs/ts/common -w libs/ts/core
 
 # for docker containers
-docker compose exec -w /usr/src/app learning-paths npm run build -w libs/contracts -w libs/ts/common
+docker compose exec -w /usr/src/app learning-paths npm run build -w libs/contracts -w libs/ts/common -w libs/ts/core
 
-docker compose exec -w /usr/src/app api-gateway npm run build -w libs/contracts -w libs/ts/common
+docker compose exec -w /usr/src/app api-gateway npm run build -w libs/contracts -w libs/ts/common -w libs/ts/core
 
 docker compose restart learning-paths api-gateway
