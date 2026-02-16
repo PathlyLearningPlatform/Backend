e418b213c212c17fd9846de7fc311d6bb3950440
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-02-16 19:48:12 +0100

    removed comment

--- scripts/win/setup-env.bat
+++ scripts/win/setup-env.bat
@@ -1,4 +1,3 @@
-REM filepath: /home/kamil/dev/projects/pathly/backend/scripts/win/setup-env.bat
 @echo off
 
 set "microservices=infra\auth infra\auth\db infra\reverse-proxy services\api-gateway services\learning-paths services\learning-paths\db"
