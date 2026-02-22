4c4a1a62d8656f6f52c1f907e2f56d279df68225
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-02-22 20:06:36 +0100

    module setup

--- services/progress/src/app.module.ts
+++ services/progress/src/app.module.ts
@@ -1,6 +1,30 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from './infra/db/db.module';
+import { ConfigModule } from '@nestjs/config';
+import { validateConfig } from './infra/config';
+import {
+	AppLoggerModule,
+	RpcRequestInterceptor,
+} from '@pathly-backend/common/index.js';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-	imports: [],
+	imports: [
+		DbModule,
+		ConfigModule.forRoot({
+			validate: validateConfig,
+			isGlobal: true,
+			ignoreEnvFile: true,
+		}),
+		AppLoggerModule.register({
+			isGlobal: true,
+		}),
+	],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: RpcRequestInterceptor,
+		},
+	],
 })
 export class AppModule {}
