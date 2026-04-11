807407c5f62991061288180f2a0e5426a47fe5f5
Kamil Abbasi <kamil.abbasi.official@gmail.com>
2026-04-11 19:21:21 +0200

    improvements

--- scripts/linux/start.local.sh
+++ scripts/linux/start.local.sh
@@ -1,4 +1,3 @@
 docker compose -f compose.local.yaml up -d --build
 
-docker compose -f compose.local.yaml exec learning-paths npm run db:migrate -w learning-paths
-docker compose -f compose.local.yaml exec progress npm run db:migrate -w progress
+docker compose -f compose.local.yaml exec api npm run db:migrate -w api
